c98345e5833b526b0a34d57f3788013db6d203ca
thiago716 <RM360138@fiap.com.br>
2025-02-08 14:53:16 -0300

    Adiciona estrutura básica do projeto, incluindo entidades, repositórios e serviços para gerenciamento de contatos e discagem direta à distância

---
+++ 01_WebApi/Program.cs
@@ -0,0 +1,26 @@
+using WebApi.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.ConfigureControllers();
+builder.ConfigureSwagger();
+builder.ConfigureDbContext();
+builder.ConfigureServices();
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
---
+++ Application/Service/ContactService.cs
@@ -0,0 +1,26 @@
+﻿using Application.Service.Base;
+using Application.Service.Interface;
+using Core.Entity;
+using Core.Repository.Interface;
+
+namespace Application.Service;
+public class ContactService : BaseService<Contact>, IContactService
+{
+    private readonly IContactRepository _contactRepository;
+    private readonly IDirectDistanceDialingRepository _directDistanceDialingRepository;
+
+    public ContactService(IContactRepository contactRepository, IDirectDistanceDialingRepository directDistanceDialingRepository) : base(contactRepository)
+    {
+        _contactRepository = contactRepository;
+        _directDistanceDialingRepository = directDistanceDialingRepository; 
+    }
+
+    public async Task<IList<Contact>> GetAllByDddAsync(int dddId)
+    {
+        var ddd = await _directDistanceDialingRepository.GetByIdAsync(dddId);
+
+        return ddd is null
+            ? throw new ArgumentException("Invalid Direct Distance Dialing Id")
+            : await _contactRepository.GetAllByDddAsync(dddId);
+    }
+}
